pmem/run_qemu

commit a1ac763bef57db372d4d0516b590b27df1b8f7bf

run_qemu: Initialize variables with presets then user provided
A user is able to provide values for parameters via the command line.
Initialize the environment with defaults before using the user
provided values to ensure that the user provided values are respected
and not overwritten by the defaults.

Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

run_qemu.sh
@@ -244,11 +244,11 @@ process_options_logic()
 		dispmode="-nographic"
 	fi
 
+	set_topo_presets "$_arg_preset"
 	num_nvmes="$_arg_nvmes"
 	num_nodes="$_arg_nodes"
 	num_mems="$_arg_mems"
 	num_pmems="$_arg_pmems"
-	set_topo_presets "$_arg_preset"
 
 	if [[ $_arg_nfit_test == "on" ]]; then
 		if (( _arg_quiet < 3 )); then

commit eb1071db8802d361b8e3b78094ca50256c96c34d

run_qemu: Fix topology command line settings
Users are able to change the topology settings of number of memory
devices, pmem devices, nvmems, etc in two different ways.  First by use of
a preset of tiny, small, med, etc.  And secondly via individual options.

The preset option was broken in a1ac763bef57 because it would always
override the preset values with the defaults of the individual cmd line
options.

Fix the preset topology settings by making the individual options
override the preset settings only if specified.  This means the
individual options no longer have a specified preset.  Include nvmes as
part of the set of topology options for consistency.

Fixes: a1ac763bef57 ("run_qemu: Initialize variables with presets then user provided")
Cc: Reinette Chatre <reinette.chatre@intel.com>
Signed-off-by: Ira Weiny <ira.weiny@intel.com>

parser_generator.m4
@@ -6,13 +6,13 @@ exit 11  #)Created by argbash-init v2.9.0
 # Rearrange the order of options below according to what you would like to see in the help message.
 # ARG_OPTIONAL_SINGLE([rootfs], , [Non-default rootfs image to use], [root.img])
 # ARG_OPTIONAL_SINGLE([rebuild], [r], [Choose one of:\n wipe: wipe builddir, removing package cache\n img: rebuild rootfs from scratch and kernel\n kmod: rebuild kernel, update rootfs modules\n none: just inject hmat and start qemu\n], [kmod])
-# ARG_OPTIONAL_SINGLE([nvmes], , [Number of NVMe devices to attach], [0])
-# ARG_OPTIONAL_SINGLE([nodes], , [Number of nodes (cpu + memory nodes)], [2])
-# ARG_OPTIONAL_SINGLE([mems], , [Number of memory-only nodes], [2])
-# ARG_OPTIONAL_SINGLE([pmems], , [Number of persistent memory dimms (each on a new node)], [2])
-# ARG_OPTIONAL_SINGLE([efi-mems], , [add efi_fake_mem reservations with the EFI_MEMORY_SP attribute], [1])
-# ARG_OPTIONAL_SINGLE([legacy-pmems], , [add e820 memmap=nn!ss reservations to emulate legacy pmem devices], [1])
-# ARG_OPTIONAL_SINGLE([preset], [p], [Select a preset config of the following:\n 1S  (or tiny)  :  1 nodes,  0 mems,  1 pmems\n 2S0 (or small0):  2 nodes,  0 mems,  2 pmems\n 2S  (or small) :  2 nodes,  2 mems,  2 pmems\n 2S4 (or med)   :  2 nodes,  4 mems,  4 pmems\n 4S  (or large) :  4 nodes,  4 mems,  4 pmems\n 8S  (or huge)  :  8 nodes,  8 mems,  8 pmems\n 16S (or insane): 16 nodes,  0 mems, 16 pmems\n 16Sb(or broken): 16 nodes,  0 mems, 32 pmems\n], [small])
+# ARG_OPTIONAL_SINGLE([nvmes], , [Number of NVMe devices to attach: (see preset)], [])
+# ARG_OPTIONAL_SINGLE([nodes], , [Number of nodes (cpu + memory nodes): (see preset)], [])
+# ARG_OPTIONAL_SINGLE([mems], , [Number of memory-only nodes: (see preset)], [])
+# ARG_OPTIONAL_SINGLE([pmems], , [Number of persistent memory dimms (each on a new node): (see preset)], [])
+# ARG_OPTIONAL_SINGLE([efi-mems], , [add efi_fake_mem reservations with the EFI_MEMORY_SP attribute: (see preset)], [])
+# ARG_OPTIONAL_SINGLE([legacy-pmems], , [add e820 memmap=nn!ss reservations to emulate legacy pmem devices: (see preset)], [])
+# ARG_OPTIONAL_SINGLE([preset], [p], [Select a preset config of the following:\n                  (nvmes == 0 for all presets)\n                  nodes mems pmems efi-mems legacy-pmems\n 1S  (or tiny)  :  1    0     1    0        0\n 2S0 (or small0):  2    0     2    0        0\n 2S  (or small) :  2    2     2    1        1\n 2S4 (or med)   :  2    4     4    1        2\n 4S  (or large) :  4    4     4    2        2\n 8S  (or huge)  :  8    8     8    2        2\n 16S (or insane): 16    0    16    2        2\n 16Sb(or broken): 16    0    32    2        2\n], [small])
 # ARG_OPTIONAL_SINGLE([mem-size], , [size of each system-memory device (in MiB)], [2048])
 # ARG_OPTIONAL_SINGLE([kver], , [Attempt to start a specific kver (only respected with --rebuild=none)], )
 # ARG_OPTIONAL_SINGLE([mirror], , [Use a different mirror for packages], )

run_qemu.sh
@@ -141,10 +141,11 @@ cleanup()
 
 trap cleanup EXIT
 
-set_topo_presets()
+set_topology()
 {
 	case "$1" in
 	1S|tiny)
+		num_nvmes=0
 		num_nodes=1
 		num_mems=0
 		num_pmems=1
@@ -152,6 +153,7 @@ set_topo_presets()
 		num_legacy_pmems=0
 		;;
 	2S0|small0)
+		num_nvmes=0
 		num_nodes=2
 		num_mems=0
 		num_pmems=2
@@ -159,6 +161,7 @@ set_topo_presets()
 		num_legacy_pmems=0
 		;;
 	2S|small)
+		num_nvmes=0
 		num_nodes=2
 		num_mems=2
 		num_pmems=2
@@ -166,6 +169,7 @@ set_topo_presets()
 		num_legacy_pmems=1
 		;;
 	2S4|med*)
+		num_nvmes=0
 		num_nodes=2
 		num_mems=4
 		num_pmems=4
@@ -173,6 +177,7 @@ set_topo_presets()
 		num_legacy_pmems=2
 		;;
 	4S|large)
+		num_nvmes=0
 		num_nodes=4
 		num_mems=4
 		num_pmems=4
@@ -180,6 +185,7 @@ set_topo_presets()
 		num_legacy_pmems=2
 		;;
 	8S|huge)
+		num_nvmes=0
 		num_nodes=8
 		num_mems=8
 		num_pmems=8
@@ -187,6 +193,7 @@ set_topo_presets()
 		num_legacy_pmems=2
 		;;
 	16S|insane)
+		num_nvmes=0
 		num_nodes=16
 		num_mems=0
 		num_pmems=16
@@ -194,6 +201,7 @@ set_topo_presets()
 		num_legacy_pmems=2
 		;;
 	16Sb|broken)
+		num_nvmes=0
 		num_nodes=16
 		num_mems=0
 		num_pmems=32
@@ -205,6 +213,26 @@ set_topo_presets()
 		exit 1
 		;;
 	esac
+
+	# After presets override individuals
+	if [[ "$_arg_nvmes" ]]; then
+		num_nvmes="$_arg_nvmes"
+	fi
+	if [[ "$_arg_nodes" ]]; then
+		num_nodes="$_arg_nodes"
+	fi
+	if [[ "$_arg_mems" ]]; then
+		num_mems="$_arg_mems"
+	fi
+	if [[ "$_arg_pmems" ]]; then
+		num_pmems="$_arg_pmems"
+	fi
+	if [[ "$_arg_efi_mems" ]]; then
+		num_efi_mems="$_arg_efi_mems"
+	fi
+	if [[ "$_arg_legacy_pmems" ]]; then
+		num_legacy_pmems="$_arg_legacy_pmems"
+	fi
 }
 
 process_options_logic()
@@ -231,7 +259,7 @@ process_options_logic()
 	fi
 	if [[ $_arg_nfit_test_run == "on" ]]; then
 		_arg_nfit_test="on"
-		set_topo_presets "med"
+		set_topology "med"
 		if [[ ! $_arg_autorun ]]; then
 			_arg_autorun="$nfit_test_script"
 		fi
@@ -264,19 +292,13 @@ process_options_logic()
 		dispmode="-nographic"
 	fi
 
-	set_topo_presets "$_arg_preset"
-	num_nvmes="$_arg_nvmes"
-	num_nodes="$_arg_nodes"
-	num_mems="$_arg_mems"
-	num_pmems="$_arg_pmems"
-	num_efi_mems="$_arg_efi_mems"
-	num_legacy_pmems="$_arg_legacy_pmems"
+	set_topology "$_arg_preset"
 
 	if [[ $_arg_nfit_test == "on" ]]; then
 		if (( _arg_quiet < 3 )); then
 			printf "setting preset to 'med' for nfit_test\n"
 		fi
-		set_topo_presets "med"
+		set_topology "med"
 	fi
 	if [[ "$_arg_mirror" ]]; then
 		mkosi_opts+=(-m "$_arg_mirror")